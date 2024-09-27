e36570983e56f5d01f0731f924f93e5c1924ac59
Madhavan0803 <Madhavanraj123@gmail.com>
2024-09-28 02:34:33 +0530

    updated all files

--- src/components/StudentContextProvider.jsx
+++ src/components/StudentContextProvider.jsx
@@ -5,12 +5,12 @@ const StudentContext = createContext()
 function StudentContextProvider (data) {
 
     const [Student, setStudent] = useState([
-        { id: 1, name: "Bhrath", fav: false },
-        { id: 2, name: "Praveen", fav: false },
-        { id: 3, name: "Kumar", fav: false },
-        { id: 4, name: "Ramya", fav: false },
-        { id: 5, name: "Monica", fav: false },
-        { id: 6, name: "Priya", fav: false }
+        { id: 1, name: "Maddy", fav: false },
+        { id: 2, name: "Allen", fav: false },
+        { id: 3, name: "Rajesh", fav: false },
+        { id: 4, name: "Arfath", fav: false },
+        { id: 5, name: "Raghul", fav: false },
+        { id: 6, name: "Praveen", fav: false }
     ])
 
     return (
